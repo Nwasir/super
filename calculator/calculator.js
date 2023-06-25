45359803de8757dab39a62e5e7aa006216f84b76
nwasir <uko20001@byui.edu>
2023-06-25 01:20:42 +0100

    square, square root and percentage

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,17 +1,32 @@
 function appendToDisplay(value) {
-    document.querySelector('#display').value += value;
+  document.querySelector("#display").value += value;
+}
+
+function clearDisplay() {
+  document.querySelector("#display").value = "";
+}
+
+function percentage() {
+  const percent = document.querySelector("#display").value;
+  const result = percent / 100;
+  document.querySelector("#display").value = result;
+}
+function squareRoot() {
+  const squareRT = document.querySelector("#display").value;
+  const result = Math.sqrt(squareRT);
+  document.querySelector("#display").value = result;
+}
+function square() {
+  const sq = document.querySelector("#display").value;
+  const result = sq ** 2;
+  document.querySelector("#display").value = result;
+}
+
+function calculate() {
+  try {
+    const result = eval(document.querySelector("#display").value);
+    document.querySelector("#display").value = result;
+  } catch (error) {
+    document.querySelector("#display").value = "Error";
   }
-  
-  function clearDisplay() {
-    document.querySelector('#display').value = '';
-  }
-  
-  function calculate() {
-    try {
-      const result = eval(document.querySelector('#display').value);
-      document.querySelector('#display').value = result;
-    } catch (error) {
-      document.querySelector('#display').value = 'Error';
-    }
-  }
-  
+}
