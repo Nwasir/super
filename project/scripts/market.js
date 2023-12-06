08495b07f36ed63ed1ef5d11d53d6a123c67877d
Nwasir <uko20001@byui.edu>
2023-12-06 07:41:33 +0100

    added link to the section

--- project/scripts/market.js
+++ project/scripts/market.js
@@ -1,92 +1,118 @@
+// market.js
 
-/* Declare and initialize global variables */
+import { displayItems, getItems } from "./marketFunctions.js";
+
+// Declare and initialize global variables
 const myOutfit = document.querySelector("#outfit");
+const url = "../json/collection.json";
 
-let clothingList = [];
-
-/* async displayTemples Function */
-function displayItems(data) {
-    data.clothing.forEach((items) => {
-        const section = document.createElement("section");
-        const h3 = document.createElement("h3");
-        const h4 = document.createElement("h4");
-        const images = document.createElement("img");
-        images.setAttribute("src", items.images);
-        images.setAttribute("alt", items.name);
-        h3.textContent = items.name;
-        h4.textContent = items.prices;
-        h4.textContent = items.sizes;
-
-        section.appendChild(h3);
-        section.appendChild(images);
-        section.appendChild(h4);
-
-        myOutfit.appendChild(section);
-    });
-};
-
-url = "../json/collection.json"
-// url = "https://run.mocky.io/v3/78e95997-f1be-4719-a0a5-036fb2c5af67";
-async function getItmes() {
-    let response = await fetch(url);
-    if (response.ok) {
-        data = await response.json();
-        displayItems(data);
-    }
-
-    console.log(response);
-};
-
-/* reset Function */
-const reset = () => {
-    myOutfit.innerHTML = "";
-};
-
-/* sortBy Function */
-
-function sortBy(items) {
-    reset();
-
-    const filter = document.querySelector("#sortBy").value;
-
-    switch (filter) {
-        case "shirt":
-            displayItems(filterItems(items, "shirts"));
-            break;
-
-        case "suit":
-            displayItems(filterItems(items, "suit"));
-            break;
-
-        case "agbada":
-            displayItems(filterItems(items, "agbada"));
-            break;
-
-        case "trousers":
-            displayItems(filterItems(items, "trousers"));
-            break;
-
-        case "senator":
-            displayItems(filterItems(items, "senator"));
-            break;
-
-        case "shoes":
-            displayItems(filterItems(items, "shoes"));
-            break;
-
-        case "all":
-            displayItems(items);
-            break;
+// Main function
+async function initializeMarket() {
+    const data = await getItems(url);
+    if (data) {
+        displayItems(data, myOutfit);
     }
 }
 
-function filterItems(items, keyword) {
-    return items.filter((item) => item.name.toLowerCase().includes(keyword.toLowerCase()));
+// Call the main function
+initializeMarket();
 
-};
-/* Event Listener */
-document.querySelector("#sortBy").addEventListener("change", () => {
-    sortBy(clothingList);
-});
+// /* Declare and initialize global variables */
+// const myOutfit = document.querySelector("#outfit");
 
-getItmes();
+// let clothingList = [];
+
+// /* async displayTemples Function */
+// async function displayItems(data) {
+//     data.clothing.forEach((items) => {
+//         const section = document.createElement("section");
+//         const h3 = document.createElement("h3");
+//         const h4 = document.createElement("h4");
+//         const h5 = document.createElement("h5");
+//         const images = document.createElement("img");
+//         images.setAttribute("src", items.images);
+//         images.setAttribute("alt", items.name);
+//         h3.textContent = items.name;
+//         h4.textContent = `Price: ${items.prices}`;
+//         h5.textContent = `Size: ${items.sizes}`;
+
+//         section.appendChild(h3);
+//         section.appendChild(images);
+//         section.appendChild(h4);
+//         section.appendChild(h5);
+
+//         myOutfit.appendChild(section);
+//     });
+// };
+
+// url = "../json/collection.json"
+// // url = "https://run.mocky.io/v3/78e95997-f1be-4719-a0a5-036fb2c5af67";
+// async function getItems() {
+//     try {
+//         let response = await fetch(url);
+//         if (response.ok) {
+//             data = await response.json();
+//             displayItems(data);
+//         } else {
+//             console.error("Failed to fetch data");
+//             console.log(response);
+//         }
+//     } catch (error) {
+//         console.error("Error fetching data:", error);
+//     }
+// };
+
+// /* reset Function */
+// // const reset = () => {
+// //     myOutfit.innerHTML = "";
+// // };
+
+// // /* sortBy Function */
+
+// // function sortBy(items) {
+// //     reset();
+
+// //     const filter = document.querySelector("#sortBy").value;
+
+// //     switch (filter) {
+// //         case "shirt":
+// //             displayItems(filterItems(items, "shirts"));
+// //             break;
+
+// //         case "suit":
+// //             displayItems(filterItems(items, "suit"));
+// //             break;
+
+// //         case "agbada":
+// //             displayItems(filterItems(items, "agbada"));
+// //             break;
+
+// //         case "trousers":
+// //             displayItems(filterItems(items, "trousers"));
+// //             break;
+
+// //         case "senator":
+// //             displayItems(filterItems(items, "senator"));
+// //             break;
+
+// //         case "shoes":
+// //             displayItems(filterItems(items, "shoes"));
+// //             break;
+
+// //         case "all":
+// //             displayItems(items);
+// //             break;
+// //     }
+// // }
+
+// // function filterItems(items, keyword) {
+// //     return items.filter((item) => item.name.toLowerCase().includes(keyword.toLowerCase()));
+
+// // };
+// // /* Event Listener */
+// // document.querySelector("#sortBy").addEventListener("change", () => {
+// //     sortBy(clothingList);
+// // });
+
+// //getItmes();
+// export { displayItems, getItems };
---
+++ project/scripts/marketFunctions.js
@@ -0,0 +1,49 @@
+// marketFunctions.js
+
+async function displayItems(data, container) {
+    data.clothing.forEach((item) => {
+        const section = document.createElement("section");
+        const link = document.createElement("a");
+        link.href = item.purchaseLink; //
+
+        const h3 = document.createElement("h3");
+        const h4 = document.createElement("h4");
+        const h5 = document.createElement("h5");
+        const images = document.createElement("img");
+        const buyLink = document.createElement("a");
+
+        images.setAttribute("src", item.images);
+        images.setAttribute("alt", item.name);
+        h3.textContent = item.name;
+        h4.textContent = `Price: ${item.prices}`;
+        h5.textContent = `Size: ${item.sizes}`;
+
+        link.appendChild(h3);
+        link.appendChild(images);
+        link.appendChild(h4);
+        link.appendChild(h5);
+        link.appendChild(buyLink);
+
+        section.appendChild(link);
+        container.appendChild(section);
+    });
+}
+
+async function getItems(url) {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            return data;
+        } else {
+            console.error("Failed to fetch data");
+            return null;
+        }
+    } catch (error) {
+        console.error("Error fetching data:", error);
+        return null;
+    }
+}
+
+// Export functions
+export { displayItems, getItems };
