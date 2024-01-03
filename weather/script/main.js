540f91c8ace638e33ceeb9008cb0e03532614b56
nwasir <uko20001@byui.edu>
2024-01-03 18:00:10 +0100

    styling the weather app

--- weather/script/main.js
+++ weather/script/main.js
@@ -6,22 +6,6 @@ const weatherInfo = document.querySelector('#weather-info');
 const locationInput = document.querySelector('#location');
 const getWeatherButton = document.querySelector('#get-weather');
 
-// getWeatherButton.addEventListener('click', () => {
-//     const location = locationInput.value;
-
-//     if (!location) {
-//         alert('Please enter a location.');
-//         return;
-//     }
-
-//     getWeatherData(location, apiKey)
-//         .then(data => {
-//             displayWeatherData(data, weatherInfo);
-//         })
-//         .catch(error => {
-//             console.error('Error fetching weather data', error);
-//         });
-// });
 [getWeatherButton].forEach(button => {
     button.addEventListener('click', () => {
         const location = locationInput.value;
